4fb939cb1b7bd92f0af241f794a793b92c902149
akjmicro <akjmicro@gmail.com>
2019-05-25 10:19:06 -0500

    adding proper dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
-setup(name='kittylog',
-      version='0.1',
-      description='Food log for the kitties (Dos and Bindi)',
-      # url='http://github.com/akjmicro/kittylog',
-      author='Aaron Krister Johnson',
-      author_email='akjmicro@gmail.com',
-      license='MIT',
-      packages=['kittylog'],
-      zip_safe=False)
+setup(
+    name='kittylog',
+    version='0.1',
+    description='Food log for the kitties (Dos and Bindi)',
+    # url='http://github.com/akjmicro/kittylog',
+    author='Aaron Krister Johnson',
+    author_email='akjmicro@gmail.com',
+    license='MIT',
+    packages=['kittylog'],
+    zip_safe=False,
+    install_requires=[
+        "flask",
+        "wtforms"
+    ]
+)
