c277086fc75709e8db72d47efe33305f7b244716
akjmicro <akjmicro@gmail.com>
2019-05-25 09:43:14 -0500

    fix setup script

--- setup_db.sh
+++ setup_db.sh
@@ -4,5 +4,5 @@ if [ ! -f kittylog/kittylog.db ]; then
     echo "Creating the initial sqlite3 database"
     sqlite3 kittylog/kittylog.db \
         "CREATE TABLE kittylog(timestamp text, feeder text, kitty text, 
-            wet text, dry text, hairball text, treats text, notes text);"
+            wet text, dry text, hairball text, treats text);"
 fi
