4afb9ab996c3b847a137ff732eb20aedf7660df6
patch <akjmicro@gmail.com>
2019-09-07 20:53:16 -0500

    reflecting column name change in db setup script

--- setup_db.sh
+++ setup_db.sh
@@ -3,10 +3,10 @@
 if [ ! -f kittylog/kittylog.db ]; then
     echo "Creating the initial sqlite3 database"
     sqlite3 kittylog/kittylog.db \
-        "CREATE TABLE IF NOT EXISTS food ("
-            "timestamp text, feeder text, kitty text, wet text, dry text, "
-            "hairball text, treats text);"
-        "CREATE TABLE IF NOT EXISTS water (timestamp text, feeder text);"
-        "CREATE INDEX IF NOT EXISTS kittylog_index ON food (timestamp);"
-        "CREATE INDEX IF NOT EXISTS water_index ON water (timestamp);"
+        "CREATE TABLE food ("
+            "timestamp text,"
+            "human text, kitty text,"
+            "wet text, dry text,"
+            "hairball text, regular text);"
+        "CREATE TABLE water (timestamp text, human text);"
 fi
